a8b473cc56733554de593f288a22624cce15322b
Maydulilas <eva.carabelos@orange.fr>
2018-01-10 22:26:35 +0100

    Commentaire, no Ajax

--- Client-side/source/js/index.js
+++ Client-side/source/js/index.js
@@ -43,20 +43,15 @@ function returnValue() {
     var path = "../../../Server-side/data/dessin.svg";
     var path1 = "../../../Server-side/data/Modele1.svg";
     var path2 = "../../../Server-side/data/Modele2.svg";
+    
 
     var myPath = [path, path1, path2];
-   /* var image = document.getElementById("image");
-
-    for( var i = 0 ; i < myPath.length; i++){
-     image.setAttribute('src', myPath[i]);
-     //trouve 3 images mais ne trouve pas le path
-     //alert(image.value);
-     */
+    
     for( var i = 0 ; i < myPath.length; i++){
         var elem = document.createElement("img");
         elem.setAttribute("src", myPath[i]);
-        elem.setAttribute("height", "200");
-        elem.setAttribute("width", "200");
+        elem.setAttribute("height", "500");
+        elem.setAttribute("width", "500");
         //On récupère l'id de la div pour insérer les nouveuax élément créer dedans
         document.getElementById("placeImage").appendChild(elem);
     }
